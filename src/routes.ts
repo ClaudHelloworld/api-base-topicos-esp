aa7646991d10fa90362ef1c5a5ec2595da41bb18
Claudinei Gomes <60714768+ClaudHelloworld@users.noreply.github.com>
2023-03-04 10:36:20 -0300

    Add files via upload

---
+++ src/routes.ts
@@ -0,0 +1,19 @@
+// Importa componentes do express 
+import {Router,Request,Response} from 'express'; 
+// Instancia roteador 
+const Roteador = Router(); 
+// Define rota tipo get que, para funcionar, deve ser requisitada conforme exemplo.
+// Exemplo de requisição: localhost:4000/teste/123?num=456 
+// Onde 123 e 456 podem ser substituídos por quaisquer valores 
+Roteador.get(    
+    // URL com parêmetro :id    
+    '/teste/:id',    
+    // Função anônima com os parâmetros de tipos Request (requisição) e Response (resposta)     
+    (req:Request,res:Response)=> {        
+        // obtém query param        
+        const x = req.query.num;        
+        // obtem route param        
+        const y = req.params.id;        
+        return res.send(`Resultado: ${Number(x) + Number(y)}`);    
+    } ); 
+export default Roteador;
